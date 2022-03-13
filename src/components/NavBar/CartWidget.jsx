ed052a41b5a0be33dab2d32d9f2e0652f7c04cad
chjack10 <christianyak@gmail.com>
2022-03-12 23:24:41 -0300

    imports Optimized

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
